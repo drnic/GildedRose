78a0ee088ebdc521ffd29490b13222ae970b9d7b
Dr Nic Williams <drnicwilliams@gmail.com>
2011-05-26 16:02:46 -0700

    added spec for aged brie

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -3,41 +3,26 @@ require "rspec"
 
 app ||= GildedRose.new
 
-generic_item = app.items.first
-
 describe GildedRose do
-  
-  
-  # Daily delta of type vs attribute
-  #
-  #         | AgedBrie | Sulfuras | BackstagePasses | Conjured | Item |
-  # sell_in |   -1     |   0      |   -1            |   -1     |  -1  |
-  # quality |   +1     |   0      |    1,2,3        |   -1     |  -1  |
-  
-  # Other tests
-  # If sell_in == 0; then quality is x2 the above
-  [:sell_in, :quality]
-  [AgedBrie, Sulfuras, BackstagePasses, Conjured, Item].each do |item_type|
-  end
-  
   describe "an item" do
+    subject { app.items.find { |i| i.name == "+5 Dexterity Vest" } }
     
     it "degrades the sell_in each day" do
-      lambda { app.update_quality }.should change(generic_item, :sell_in).by(-1)
+      lambda { app.update_quality }.should change(subject, :sell_in).by(-1)
     end
     
     it "degrades the quality each day" do
-      lambda { app.update_quality }.should change(generic_item, :quality).by(-1)
+      lambda { app.update_quality }.should change(subject, :quality).by(-1)
     end
     
     it "degrades the quality twice as fast after sell_in is 0" do
-      generic_item.sell_in = 0
-      lambda { app.update_quality }.should change(generic_item, :quality).by(-2)
+      subject.sell_in = 0
+      lambda { app.update_quality }.should change(subject, :quality).by(-2)
     end
     
     it "degrades the quality twice as fast after sell_in is 0" do
-      generic_item.quality = 0
-      lambda { app.update_quality }.should_not change(generic_item, :quality)
+      subject.quality = 0
+      lambda { app.update_quality }.should_not change(subject, :quality)
     end
     
   end
